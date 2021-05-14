e84547cadb3dd6a1acf54a004a04a78cd4b97c8b
igorbukanov <sacrifall@inbox.ru>
2021-05-15 00:41:04 +0300

    fix

--- src/components/PlaceInput/PlaceInput.tsx
+++ src/components/PlaceInput/PlaceInput.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react'
+import React from 'react'
 import styled from 'styled-components';
 import PlaceList from '../PlaceList/PlaceList';
 import Input from '../../ui/Input';
